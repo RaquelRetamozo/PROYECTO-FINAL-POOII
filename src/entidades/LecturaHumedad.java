f11869880e9c3ee8314d969fd939d87641561746
RAQUEL <retamozoutaniraquel@gmail.com>
2026-06-13 22:43:34 -0500

    Clase LecturaHumedad

---
+++ src/entidades/LecturaHumedad.java
@@ -0,0 +1,48 @@
+package entidades;
+
+public class LecturaHumedad extends LecturaSensor {
+
+    //Atributos
+    private double humedadRegistrada;
+    private double humedadMin;
+    private double humedadMax;
+
+    //Constructor
+    public LecturaHumedad(String idLectura, String idSensor, ZonaAlmacen zona,
+                          double valor, double humedadMin, double humedadMax) {
+        super(idLectura, idSensor, zona, valor);
+        this.humedadRegistrada = valor;
+        this.humedadMin = humedadMin;
+        this.humedadMax = humedadMax;
+        this.setDentroRango(humedadRegistrada >= humedadMin &&
+                humedadRegistrada <= humedadMax);
+    }
+
+    //Métodos
+    public boolean estaFueraDeRango() {
+        return !isDentroRango();
+    }
+
+    @Override
+    public String getTipoLectura() {
+        return "HUMEDAD";
+    }
+
+    //Getters
+    public double getHumedadRegistrada() { return humedadRegistrada; }
+    public double getHumedadMin() { return humedadMin; }
+    public double getHumedadMax() { return humedadMax; }
+
+    //Setters
+    public void setHumedadRegistrada(double humedadRegistrada) { this.humedadRegistrada = humedadRegistrada; }
+    public void setHumedadMin(double humedadMin) { this.humedadMin = humedadMin; }
+    public void setHumedadMax(double humedadMax) { this.humedadMax = humedadMax; }
+
+    //ToString
+    @Override
+    public String toString() {
+        return super.toString() +
+                " | Humedad: " + humedadRegistrada + "%" +
+                " | Rango: " + humedadMin + "% - " + humedadMax + "%";
+    }
+}
