a7ca69232b5959e6adf58f7a194f47376c4c1edd
bryan <bryan.lex1999@gmail.com>
2026-06-23 08:44:43 -0500

    Ordenamiento en Packages, IProductoRepository, ProductoRepositoryMemoria y ILoteRepository

--- src/Model/repositorios/ILoteRepository.java
+++ src/Model/repositorios/ILoteRepository.java
@@ -1,4 +1,9 @@
-package Model.repositorios;
+import java.util.List;
 
-public class ILoteRepository {
+public interface ILoteRepository {
+    void agregar(Lote lote);
+    Lote buscarPorId(String idLote);
+    List<Lote> listarTodos();
+    void actualizar(Lote lote);
+    void eliminar(String idLote);
 }
