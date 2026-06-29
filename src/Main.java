2fe25aae536ed3457be1e68edfa2e864a9556607
YohannaCop <yohanna.copacondori@usil.pe>
2026-06-29 17:00:43 -0500

    Main: Coreccion controllers

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,9 @@ public static void Main(Strings){
     Contexto ctx = Contexto.getInstancia();
 
     //Servicios
+
+
+    // Controllers
     InventarioService inventarioService = new InventarioService(ctx.getProductoRepo());
     StockService stockService = new StockService(ctx.getStockRepo());
     LoteService loteService = new LoteService(ctx.getLoteRepo());
@@ -28,23 +31,6 @@ public static void Main(Strings){
     EstadisticaService estadisticaService = new EstadisticaService(ctx.getStockRepo(), ctx.getDespachoRepo());
     UsuarioService usuarioService = new UsuarioService(ctx.getUsuarioRepo());
     AutenticacionService autenticacionService = new AutenticacionService(ctx.getUsuarioRepo());
-    ClienteService clienteService = new ClienteService(ctx.getClienteRepo());
-
-    // Controllers
-    InventarioController inventarioController = new InventarioController(inventarioService);
-    StockController stockController = new StockController(stockService);
-    LoteController loteController = new LoteController(loteService);
-    TrazabilidadController trazabilidadController = new TrazabilidadController(trazabilidadService);
-    DespachoController despachoController = new DespachoController(despachoService);
-    ClienteController clienteController = new ClienteController(ctx.getClienteRepo());
-    MonitoreoController monitoreoController = new MonitoreoController(monitoreoService);
-    AlertaController alertaController = new AlertaController(alertaVencimientoService, alertaDesabastecimientoService, alertaAmbientalService);
-    NoConformidadController noConformidadController = new NoConformidadController(noConformidadService);
-    ReclamoController reclamoController = new ReclamoController(reclamoService);
-    ReporteController reporteController = new ReporteController(reporteService);
-    EstadisticaController estadisticaController = new EstadisticaController(estadisticaService);
-    UsuarioController usuarioController = new UsuarioController(usuarioService);
-    AutenticacionController autenticacionController = new AutenticacionController(autenticacionService);
 
     //Vistas
     LoginVista login vista = new LoginVista();
